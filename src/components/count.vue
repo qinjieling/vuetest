b619e898d6bfc4de9bf73db282da16408c294e91
qinjieling <997678858@qq.com>
2020-08-19 13:56:47 +0800

    moddify://mutations,actions,gets的使用

--- src/components/count.vue
+++ src/components/count.vue
@@ -1,17 +1,26 @@
 <template>
   <div class="wrap">
     <div class="add">
-      <h1>{{$store.state.count}}</h1>
+      <!-- <h1>{{$store.state.count}}</h1> -->
+      <h1>{{count}}</h1>
       <button @click="addHandle">+1</button>
+      <button @click="addHandle2">+N</button>
+      <button @click="addHandle3">+1 async</button>
+      <button @click="addHandle4">+N async</button>
     </div>
     <div class="add">
-      <h1>{{$store.state.count}}</h1>
-      <button @click="subHandle">-1</button>
+      <!-- <h1>{{$store.getters.showNum}}</h1> -->
+      <h1>{{showNum}}</h1>
+      <button @click="sub()">-1</button>
+      <button @click="subN(3)">-N</button>
+      <button @click="subHandle3">-1 async</button>
     </div>
   </div>
 </template>
 
 <script>
+//store中全局的值映射到当前组件
+import {mapState,mapMutations,mapActions,mapGetters} from 'vuex'
 export default {
   name: 'count',
   data () {
@@ -19,12 +28,30 @@ export default {
       // count: 'Welcome'
     }
   },
+  computed:{
+    ...mapState(['count']),
+    ...mapGetters(['showNum'])
+  },
   methods:{
+    ...mapMutations(['add','addN','sub','subN']),
+    ...mapActions(['subAsync']),
     addHandle(){
-      this.$store.commit('addN',3)
+      this.add()
     },
-    subHandle(){
-      this.$store.commit('subN',3)
+    addHandle2(){
+      //commit的作用：调用某个mutation函数
+      // this.$store.commit('addN',3)
+      this.addN(3)
+    },
+    addHandle3(){
+      //dispatch函数专门用来触发action
+      this.$store.dispatch('addAsync')
+    },
+    addHandle4(){
+      this.$store.dispatch('addNAsync',5)
+    },
+    subHandle3(){
+      this.subAsync()  //也可直接写在元素上
     }
   }
 }
--- src/store/store.js
+++ src/store/store.js
@@ -7,21 +7,44 @@ export default new Vuex.Store({
   state: {
     count:0
   },
+  //只有mutation中定义的函数才可以修改state中的数据
   mutations: {
     add(state){
       state.count ++
     },
     addN(state,step){
-      state.count = state.count + step
+      state.count += step
     },
     sub(state){
       state.count --
     },
     subN(state,step){
-      state.count = state.count - step
+      state.count -= step
     },
   },
+  //不能直接修改state中的数据
+  //必须通过context.commit()触发某个mutation才行
   actions: {
-
+    addAsync(context){
+      setTimeout(()=>{
+        context.commit('add')
+      },1000)
+    },
+    addNAsync(context,step){
+      setTimeout(()=>{
+        context.commit('addN',step)
+      },1000)
+    },
+    subAsync(context){
+      setTimeout(()=>{
+        context.commit('sub')
+      },1000)
+    },
+  },
+  //不会修改store中的原数据，对store中已有数据进行加工处理之后形成新数据，类似computed属性
+  getters:{
+    showNum:status=>{
+      return '当前最新数据：'+ status.count
+     }
   }
 })
